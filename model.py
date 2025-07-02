c07b9f2c96f17bae9893b7c35cd9f10a66d44045
Siddharth Kundu <sk31395@gmail.com>
2025-07-02 01:38:08 -0400

    Update .gitignore, refine ResidualBlock, and add training script for AudioCNN

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class ResidualBlock(nn.Module):
             in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.conv2 = nn.Conv2d(
-            out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+            out_channels, out_channels, kernel_size=3, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.shortcut = nn.Sequential()
         self.use_shortcut = stride != 1 or in_channels != out_channels
@@ -24,10 +24,10 @@ class ResidualBlock(nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = torch.relu(out)
-        out = self.conv2(x)
+        out = self.conv2(out)
         out = self.bn2(out)
         out = torch.relu(out)
-        shortcut = self.shortcut(x) if self.use_shortcuzt else x
+        shortcut = self.shortcut(x) if self.use_shortcut else x
         out_add = out + shortcut
         out = torch.relu(out_add)
 
@@ -45,11 +45,11 @@ class AudioCNN(nn.Module):
         )
         self.layer1 = nn.ModuleList([ResidualBlock(64, 64) for _ in range(3)])
         self.layer2 = nn.ModuleList(
-            [ResidualBlock(64 if i == 0 else 128, 128) for i in range(4)])
+            [ResidualBlock(64 if i == 0 else 128, 128, stride=2 if i == 0 else 1) for i in range(4)])
         self.layer3 = nn.ModuleList(
-            [ResidualBlock(128 if i == 0 else 256, 256) for i in range(6)])
+            [ResidualBlock(128 if i == 0 else 256, 256, stride=2 if i == 0 else 1) for i in range(6)])
         self.layer4 = nn.ModuleList(
-            [ResidualBlock(256 if i == 0 else 512, 512) for i in range(3)])
+            [ResidualBlock(256 if i == 0 else 512, 512, stride=2 if i == 0 else 1) for i in range(3)])
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(0.5)
